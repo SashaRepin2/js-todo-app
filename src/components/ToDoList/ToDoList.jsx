291631bc6d5b138e722322bbe6a4eff5b99f6cb1
Sasha Repin <sasharepin2016@gmail.com>
2022-06-15 23:13:29 +0300

    chahnge ToDoItem key value

--- src/components/ToDoList/ToDoList.jsx
+++ src/components/ToDoList/ToDoList.jsx
@@ -1,30 +1,43 @@
-import React from 'react';
-import ToDoItem from '../ToDoItem/ToDoItem';
-import styles from './ToDoList.module.css';
+import React from "react";
+import ToDoItem from "../ToDoItem/ToDoItem";
+import styles from "./ToDoList.module.css";
 
-const ToDoList = ({ todos, remove, complete, setEditTodo, setIsActiveModal }) => {
-	return (
-		<div className={styles.todo__list}>
-			{todos.length ? (
-				todos.map((todo, index) => {
-					return (
-						<ToDoItem
-							todo={todo}
-							key={index}
-							remove={remove}
-							change={setEditTodo}
-							setIsActiveModal={setIsActiveModal}
-							complete={complete}
-						/>
-					);
-				})
-			) : (
-				<span style={{ color: '#fff', textAlign: 'center', fontWeight: 'bold', fontSize: '25px' }}>
-					Добавьте Todo
-				</span>
-			)}
-		</div>
-	);
+const ToDoList = ({
+  todos,
+  remove,
+  complete,
+  setEditTodo,
+  setIsActiveModal,
+}) => {
+  return (
+    <div className={styles.todo__list}>
+      {todos.length ? (
+        todos.map((todo) => {
+          return (
+            <ToDoItem
+              todo={todo}
+              key={todo.id}
+              remove={remove}
+              change={setEditTodo}
+              setIsActiveModal={setIsActiveModal}
+              complete={complete}
+            />
+          );
+        })
+      ) : (
+        <span
+          style={{
+            color: "#fff",
+            textAlign: "center",
+            fontWeight: "bold",
+            fontSize: "25px",
+          }}
+        >
+          Добавьте Todo
+        </span>
+      )}
+    </div>
+  );
 };
 
 export default ToDoList;
